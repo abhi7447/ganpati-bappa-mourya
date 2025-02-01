eb7c2202688de6667001ae14bb906a9c986ac37c
abhi7447 <abhilashmataghare22@gmail.com>
2025-02-01 17:08:10 +0530

    init commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+inputBox.onclick=()=>{
+    alert('welcome');
+}
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("you must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+        inputBox.value="";
+    }
+}
